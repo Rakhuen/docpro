d12b84eb1586b7c90691e53c15be06e9bd942d4d
Nesia Yunindya <n.yunindya@homecredit.co.id>
2020-05-14 00:39:18 +0700

    fix header Button function

--- src/Components/HeaderButton.js
+++ src/Components/HeaderButton.js
@@ -1,23 +1,18 @@
-import React from 'react';
-import './HeaderButton.css';
+import React from "react";
+import "./HeaderButton.css";
 
-const HeaderMenu = props => {
-    const { btnKiri, btnKanan, functionKiri} = props;
-    return(
-       
-        <div className="Header2Container">
-            <button className="buttonKiri" onClick =  {functionKiri}>{btnKiri}</button>
-            <button className="buttonKanan" onClick>{btnKanan}</button>
-        </div>
-        
-    
-    
-    
-    
-    );
-}
-
-
-
+const HeaderMenu = (props) => {
+  const { btnKiri, btnKanan, functionKiri, functionKanan } = props;
+  return (
+    <div className="Header2Container">
+      <button className="buttonKiri" onClick={functionKiri}>
+        {btnKiri}
+      </button>
+      <button className="buttonKanan" onClick={functionKanan}>
+        {btnKanan}
+      </button>
+    </div>
+  );
+};
 
 export default HeaderMenu;
