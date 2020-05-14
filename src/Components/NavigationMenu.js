66d1e150945b50d96f7985ea979d97ca751e98ba
Nesia Yunindya <n.yunindya@homecredit.co.id>
2020-05-14 23:50:18 +0700

    make logout

--- src/Components/NavigationMenu.js
+++ src/Components/NavigationMenu.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./navigation.css";
 import { NavLink } from "react-router-dom";
+import Courage from "../asset/courage.jpg";
 
 const NavigationMenu = () => {
   const homeActive = window.location.pathname === "/home";
@@ -8,50 +9,76 @@ const NavigationMenu = () => {
   const historyActive = window.location.pathname === "/history";
   const inputbiayaActive = window.location.pathname === "/inputbiaya";
 
-  return (
-    <div className="NavigationCon">
-      <div className="LogoContainer">
-        <div className="Logo">DocPro</div>
-      </div>
+  const Footer = (props) => {
+    const { username } = props;
 
-      <div className="MenuContainer">
-        <NavLink
-          to="/home"
-          className={homeActive ? "menuActive" : "menuInactive"}
-        >
-          Jadwal
-        </NavLink>
+    const handleLogout = () => {
+      alert("logout");
+    };
 
-        <NavLink
-          to="/pasien"
-          className={patientActive ? "menuActive" : "menuInactive"}
-        >
-          Data Pasien
-        </NavLink>
-
-        <NavLink
-          to="/history"
-          className={historyActive ? "menuActive" : "menuInactive"}
-        >
-          History
-        </NavLink>
-
-        <NavLink
-          to="/inputbiaya"
-          className={inputbiayaActive ? "menuActive" : "menuInactive"}
-        >
-          Input Biaya
-        </NavLink>
-
-      </div>
-
+    return (
       <div className="footer">
-        <div className="FotoProfile">foto</div>
-        <div className="NameAndSignout">
-          <div className="Nama">Budi</div>
-          <div className="btnSignout">Sign out</div>
+        <hr></hr>
+        <div style={{ display: "flex" }}>
+          <div className="photoContainer">
+            <img
+              className="profilePhoto"
+              src={Courage}
+              alt="profilePhoto"
+            ></img>
+          </div>
+          <div className="signoutContainer">
+            <div className="username">{username} </div>
+            <NavLink to="/">
+              <button className="btnSignout" onClick={() => handleLogout}>
+                Sign out
+              </button>
+            </NavLink>
+          </div>
         </div>
       </div>
+    );
+  };
+
+  return (
+    <div className="NavigationCon">
+      <div className="upperNavContainer">
+        <div className="LogoContainer">
+          <div className="Logo">DocPro</div>
+        </div>
+
+        <div className="MenuContainer">
+          <NavLink
+            to="/home"
+            className={homeActive ? "menuActive" : "menuInactive"}
+          >
+            Jadwal
+          </NavLink>
+
+          <NavLink
+            to="/pasien"
+            className={patientActive ? "menuActive" : "menuInactive"}
+          >
+            Data Pasien
+          </NavLink>
+
+          <NavLink
+            to="/history"
+            className={historyActive ? "menuActive" : "menuInactive"}
+          >
+            History
+          </NavLink>
+
+          <NavLink
+            to="/inputbiaya"
+            className={inputbiayaActive ? "menuActive" : "menuInactive"}
+          >
+            Input Biaya
+          </NavLink>
+        </div>
+      </div>
+
+      <Footer username="Drg. Siapa siapa"></Footer>
     </div>
   );
 };
