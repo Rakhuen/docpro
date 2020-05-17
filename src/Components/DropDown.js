3379b949548086f957f961331a51cf9653f3866f
Nandhika Pratama Putra <nandhikapratamaputra@Nandhikas-MacBook-Pro.local>
2020-05-17 22:48:28 +0700

    test

--- src/Components/DropDown.js
+++ src/Components/DropDown.js
@@ -27,7 +27,7 @@ function Dropdown({ title, items = [], multiSelect = false }) {
     }
     return false;
   }
-}
+
 
 return (
   <div className="dd=wrapper">
@@ -59,5 +59,5 @@ return (
     )}
   </div>
 );
-
+        };        
 export default Dropdown;
