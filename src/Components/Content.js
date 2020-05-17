9566896deade325c9183dfafb9f71400819866da
Nesia Yunindya <n.yunindya@homecredit.co.id>
2020-05-17 22:45:47 +0700

    fix layout

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,55 +1,54 @@
-import React from 'react';
+import React from "react";
 import "./Content.css";
 
+const Card = (props) => {
+  const {
+    nama,
+    perawatan,
+    jam,
+    image,
+    imageDelete,
+    btnFinish,
+    btnCancel,
+    functionFinish,
+    functionCancel,
+    btnViewDetails,
+    functionDetails,
+  } = props;
+  return (
+    <div className="CardContainer">
+      <div className="DeleteContainer">
+        <img className="Delete" src={imageDelete} />
+      </div>
+      <div className="FotodanNama">
+        <img className="Foto2" src={image} />
 
-const Card = props => {
-    const { nama, perawatan, jam, image, imageDelete,btnFinish,btnCancel,functionFinish,functionCancel,btnViewDetails,functionDetails} = props;
-    return(
-        <div className="CardContainer">
-            <div className="DeleteContainer">
-            <img className="Delete" src = {imageDelete}/>
-            </div>
-            <div className="FotodanNama">
-                <img className="Foto2" src = {image} />
-                
-                <div className="Nama">
-                    <div className="Atas">{nama}</div>
-                    <div className="Bawah">{perawatan}</div>
-                </div>
-            </div>
-
-            <div className="CheckOrCancelContainer">
-                <div className="JamJadwal">{jam}</div>
-
-                <div className="CnC">
-                <button className="btnCheck" onClick={functionFinish}>
-                     {btnFinish}
-                </button>
-                <button className="btnCancel" onClick={functionCancel}>
-                     {btnCancel}
-                </button>
-                   
-                </div>
-            </div>
-
-            <div className="DetailsContainer">
-            <button className="btnDetails" onClick={functionDetails}>
-                     {btnViewDetails}
-                </button>
-                
-            </div>
-
+        <div className="Nama">
+          <div className="Atas">{nama}</div>
+          <div className="Bawah">{perawatan}</div>
         </div>
-        
+      </div>
 
+      <div className="CheckOrCancelContainer">
+        <div className="JamJadwal">{jam}</div>
 
-    );
+        <div className="CnC">
+          <button className="btnCheck" onClick={functionFinish}>
+            {btnFinish}
+          </button>
+          <button className="btnCancel" onClick={functionCancel}>
+            {btnCancel}
+          </button>
+        </div>
+      </div>
 
-
-
+      <div className="DetailsContainer">
+        <button className="btnDetails" onClick={functionDetails}>
+          {btnViewDetails}
+        </button>
+      </div>
+    </div>
+  );
 };
-
-
-
 
 export default Card;
--- src/Components/DropDown.js
+++ src/Components/DropDown.js
@@ -27,37 +27,37 @@ function Dropdown({ title, items = [], multiSelect = false }) {
     }
     return false;
   }
-}
 
-return (
-  <div className="dd=wrapper">
-    <div
-      tabIndex={0}
-      className="dd-header"
-      role="button"
-      onKeyPress={() => toggle(!open)}
-      onClick={() => toggle(!open)}
-    >
-      <div className="dd-header_title">
-        <p className="dd-header_title--bold">{title}</p>
+  return (
+    <div className="dd=wrapper">
+      <div
+        tabIndex={0}
+        className="dd-header"
+        role="button"
+        onKeyPress={() => toggle(!open)}
+        onClick={() => toggle(!open)}
+      >
+        <div className="dd-header_title">
+          <p className="dd-header_title--bold">{title}</p>
+        </div>
+        <div className="dd-header_action">
+          <p>{open ? "Close" : "Open"}</p>
+        </div>
       </div>
-      <div className="dd-header_action">
-        <p>{open ? "Close" : "Open"}</p>
-      </div>
+      {open && (
+        <ul className="dd-list">
+          {items.map((item) => (
+            <li className="dd-list-item" key={item.id}>
+              <button type="button" onClick={() => handleOnClick(item)}>
+                <span>{item.value}</span>
+                <span>{isItemInSelection(item) && "Selected"} </span>
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
-    {open && (
-      <ul className="dd-list">
-        {items.map((item) => (
-          <li className="dd-list-item" key={item.id}>
-            <button type="button" onClick={() => handleOnClick(item)}>
-              <span>{item.value}</span>
-              <span>{isItemInSelection(item) && "Selected"} </span>
-            </button>
-          </li>
-        ))}
-      </ul>
-    )}
-  </div>
-);
+  );
+}
 
 export default Dropdown;
