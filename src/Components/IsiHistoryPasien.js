8e0152ddd8470fa296e323453f23ef50c11a53bb
Nesia Yunindya <n.yunindya@homecredit.co.id>
2020-05-20 00:28:09 +0700

    fix input page

--- src/Components/IsiHistoryPasien.js
+++ src/Components/IsiHistoryPasien.js
@@ -1,33 +1,30 @@
 import React from "react";
-import "../View/HistoryPasien.css"
-
+import "../View/HistoryPasien.css";
 
 const IsiHistory = (props) => {
-    const {
-      tanggalHistory,
-      namaHistory,
-      keluhanHistory,
-      penangananHistory,
-    } = props;
-    return (
-      <div className="IsiKeterangan">
-        <div className="ItemTanggal">
-          <div className="IsiTanggal"> {tanggalHistory}</div>
-        </div>
-        
-        <div className="Item">
-          <div className="IsiKeluhanHistory">{keluhanHistory}</div>
-        </div>
-        <div className="Item">
-          <div className="IsiPenangananHistory">{penangananHistory}</div>
-        </div>
-        <div className="ItemNama">
-          <div className="IsiNamaHistory">{namaHistory}</div>
-        </div>
+  const {
+    tanggalHistory,
+    namaHistory,
+    keluhanHistory,
+    penangananHistory,
+  } = props;
+  return (
+    <div className="IsiKeterangan">
+      <div className="ItemTanggal">
+        <div className="IsiTanggal"> {tanggalHistory}</div>
       </div>
-    );
-  };
-  
-  
 
-  export default IsiHistory;
+      <div className="Item">
+        <div className="IsiKeluhanHistory">{keluhanHistory}</div>
+      </div>
+      <div className="Item">
+        <div className="IsiPenangananHistory">{penangananHistory}</div>
+      </div>
+      <div className="ItemNama">
+        <div className="IsiNamaHistory">{namaHistory}</div>
+      </div>
+    </div>
+  );
+};
+
+export default IsiHistory;
